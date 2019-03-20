1ff06f823f9d5789651b1b8d53a09fe2c7f09d6d
mihailLukyaniuk <43263508+mihailLukyaniuk@users.noreply.github.com>
2019-03-20 07:02:19 +0300

    Add files via upload

---
+++ src/by/epam/javatraining/mikhaillukyaniuk/tasks/maintask01/model/VectorInitialize.java
@@ -0,0 +1,26 @@
+package by.epam.javatraining.mikhaillukyaniuk.tasks.maintask01.model;
+
+import java.util.*;
+
+public class VectorInitialize {
+	private double[] array;
+
+	public VectorInitialize(double[] array) {
+		this.array = array;
+	}
+
+	public double[] getArr() {
+		return array;
+	}
+
+	public void setElements(double[] arr) {
+		this.array = array;
+	}
+    
+       
+
+        
+
+        
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Миша
+ *
+ */
+module Project1 {
+}
