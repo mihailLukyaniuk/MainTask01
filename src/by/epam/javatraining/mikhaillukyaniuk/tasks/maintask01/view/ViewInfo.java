a5f005c8f3b0a5e56b40736a9d27ab89d8288dc0
mihailLukyaniuk <43263508+mihailLukyaniuk@users.noreply.github.com>
2019-03-20 07:31:16 +0300

    Update ViewInfo.java

--- src/by/epam/javatraining/mikhaillukyaniuk/tasks/maintask01/view/ViewInfo.java
+++ src/by/epam/javatraining/mikhaillukyaniuk/tasks/maintask01/view/ViewInfo.java
@@ -1,3 +1,9 @@
+/**
+ * Created by Mikhail Lukyaniuk 19.03.2019.
+ * EPAM java course Main Task 01
+ * Sorting and Searching Algorithms. Work with vector
+ * View part
+ */
 package by.epam.javatraining.mikhaillukyaniuk.tasks.maintask01.view;
 
 public class ViewInfo {
