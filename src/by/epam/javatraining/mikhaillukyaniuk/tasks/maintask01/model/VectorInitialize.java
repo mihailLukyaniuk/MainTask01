368a64f791e5e8c0e4f7d3d327e041100646a9d2
mihailLukyaniuk <43263508+mihailLukyaniuk@users.noreply.github.com>
2019-03-21 18:01:58 +0300

    Add files via upload

---
+++ src/by/epam/javatraining/mikhaillukyaniuk/tasks/maintask01/model/VectorInitialize.java
@@ -0,0 +1,36 @@
+/**
+ * Created by Mikhail Lukyaniuk 19.03.2019.
+ * EPAM java course Main Task 01
+ * Sorting and Searching Algorithms. Work with vector
+ * Model part
+ */
+package by.epam.javatraining.mikhaillukyaniuk.tasks.maintask01.model;
+
+public class VectorInitialize {
+	
+	private double[] array;
+
+	public VectorInitialize(double[] array) {
+		this.array = array;
+	}
+	
+	public VectorInitialize() {
+	}
+	
+	public double[] getArray() {
+		return array;
+	}
+
+	public void setArray(double[] array) {
+		this.array = array;
+	}
+
+
+    
+       
+
+        
+
+        
+
+}
